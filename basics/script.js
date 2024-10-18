26f784ae5086b85984af03aa2c9e6fc011e2ced4
Usman Javed <usmanjaved0816@gmail.com>
2024-10-18 12:32:47 +0500

    make a light bulb

--- basics/script.js
+++ basics/script.js
@@ -7,16 +7,36 @@
 
 
 
-let a = document.querySelector('h1') ;
-a.innerHTML = 'good by pakistan';
+// let a = document.querySelector('h1') ;
+// a.innerHTML = 'good by pakistan';
 
-a.style.backgroundColor = 'black';
-a.style.color = 'red';
+// a.style.backgroundColor = 'black';
+// a.style.color = 'red';
 
 
-a.addEventListener('click', function(){
-    //    console.log('hii');
-    a.innerHTML = 'hope you well come again ';
-    a.style.color = '#fff'
-    a.style.backgroundColor = "red";
+// a.addEventListener('click', function(){
+//     //    console.log('hii');
+//     a.innerHTML = 'hope you well come again ';
+//     a.style.color = '#fff'
+//     a.style.backgroundColor = "red";
+// });
+
+
+
+// bulb 
+
+let bulb = document.querySelector ('.bulb');
+let btn = document.querySelector ('button');
+let flag = 0;
+btn.addEventListener ('click', function (){
+  if (flag === 0) {  
+bulb.style.backgroundColor = 'yellow';
+btn.innerHTML = 'OFF';
+flag = 1;
+  }
+  else {
+    bulb.style.backgroundColor = '#fff';
+btn.innerHTML = 'ON';
+flag = 0;
+  }
 });
