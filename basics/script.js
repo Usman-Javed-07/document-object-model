5529bdc1f2c07746d028dbb4dbd14c5a5a9de77d
Usman Javed <usmanjaved0816@gmail.com>
2024-10-18 14:56:59 +0500

    textcontent vs innerHtml

--- basics/script.js
+++ basics/script.js
@@ -25,18 +25,40 @@
 
 // bulb 
 
-let bulb = document.querySelector ('.bulb');
-let btn = document.querySelector ('button');
-let flag = 0;
-btn.addEventListener ('click', function (){
-  if (flag === 0) {  
-bulb.style.backgroundColor = 'yellow';
-btn.innerHTML = 'OFF';
-flag = 1;
-  }
-  else {
-    bulb.style.backgroundColor = '#fff';
-btn.innerHTML = 'ON';
-flag = 0;
-  }
-});
+// let bulb = document.querySelector ('.bulb');
+// let btn = document.querySelector ('button');
+// let flag = 0;
+// btn.addEventListener ('click', function (){
+//   if (flag === 0) {  
+// bulb.style.backgroundColor = 'yellow';
+// btn.innerHTML = 'OFF';
+// flag = 1;
+//   }
+//   else {
+//     bulb.style.backgroundColor = '#fff';
+// btn.innerHTML = 'ON';
+// flag = 0;
+//   }
+// });
+
+
+// 
+// quaryselectorall
+
+// let h = document.querySelectorAll('h1');
+// h.forEach(function(e){
+//    console.log(e)
+// });
+
+
+// textcontant vs innerHtml
+
+
+let box = document.getElementById ('box')
+// box.innerHTML = '<h1> hello world  </h1>'
+box.textContent = '<h1> hello world  </h1>'
+
+
+
+
+
